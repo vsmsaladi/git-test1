92780d3b4d380c83dc7852408909b64c7ac2a3ed
Santosh Mohan Saladi <vsaladi@uncc.edu>
2023-09-01 20:17:37 -0400

    My First Commit

---
+++ my-server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.write('Hello World from Santosh!');
+  res.end();
+}).listen(3000, function(){
+ console.log("Server running at port 3000");
+});
